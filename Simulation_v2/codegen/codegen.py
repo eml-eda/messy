66ce6106d4cf315f6f127c245e02aba237ba4466
Checco1 <s296579@studenti.polito.it>
2023-09-20 19:44:04 +0200

    Added other templates

--- Simulation_v2/codegen/codegen.py
+++ Simulation_v2/codegen/codegen.py
@@ -11,34 +11,55 @@ def main(input_file, template_dir, output_dir):
     with open(input_file, "r") as f:
         settings = json.load(f)
 
+    i = 1
+
+    for sensor in settings["sensors"]:
+        sensor["ID"] = i
+        sensor["reg_name"] = sensor["name"].upper() + "_REG_DIMENSION"
+        sensor["v_name"] = sensor["name"].upper() + "_VOLTAGE"
+        i += 1
+    
+    print(json.dumps(settings))
+
     header_dir = output_dir / 'inc'
     header_dir.mkdir(parents=True, exist_ok=True)
     src_dir = output_dir / 'src'
     src_dir.mkdir(parents=True, exist_ok=True)
 
+    #Config.h statement
+    codegen_func.config_gen(settings, template_dir, header_dir)
+
+    #Functional Bus statement
+#    codegen_func.sys_functional_bus_h_gen(template_dir,header_dir)
+#    codegen_func.sys_functional_bus_cpp_gen(settings,template_dir,src_dir)
+#
+#    #Power Bus statement
+#    codegen_func.sys_power_bus_h_gen(template_dir,header_dir)
+#    codegen_func.sys_power_bus_cpp_gen(template_dir,src_dir)
+
     #Battery selection statement 
-    if settings["battery"] == "peukert":
-        codegen_func.battery_peukert_h_gen(template_dir, header_dir)
-        codegen_func.battery_peukert_cpp_gen(template_dir,src_dir)
-    elif settings["battery"] == "circuit":
-        codegen_func.battery_circuit_h_gen(template_dir, header_dir)
-        codegen_func.battery_circuit_cpp_gen(template_dir,src_dir)
-    
-    #Battery converter statement 
-    codegen_func.battery_converter_h_gen(template_dir, header_dir)
-    codegen_func.battery_converter_cpp_gen(template_dir, src_dir)
-
-    #Sensor statement
-    #for sensor in settings['sensors']:
-        #codegen_func.sensor_func_h_gen(sensor, template_dir, header_dir)
-        #codegen_func.sensor_func_cpp_gen(sensor, template_dir, src_dir)
-        #if sensor['power'] == True:
-            #codegen_func.sensor_power_h_gen(sensor, template_dir, header_dir)
-            #codegen_func.sensor_power_cpp_gen(sensor, template_dir, src_dir)
-
-    # Generate Load_converter files
-    codegen_func.load_converter_h_gen(template_dir, header_dir)
-    codegen_func.load_converter_cpp_gen(template_dir, src_dir)
+#    if settings["battery"] == "peukert":
+#        codegen_func.battery_peukert_h_gen(template_dir, header_dir)
+#        codegen_func.battery_peukert_cpp_gen(template_dir,src_dir)
+#    elif settings["battery"] == "circuit":
+#        codegen_func.battery_circuit_h_gen(template_dir, header_dir)
+#        codegen_func.battery_circuit_cpp_gen(template_dir,src_dir)
+#    
+#    #Battery converter statement 
+#    codegen_func.battery_converter_h_gen(template_dir, header_dir)
+#    codegen_func.battery_converter_cpp_gen(template_dir, src_dir)
+#
+#    #Sensor statement
+#    for sensor in settings['sensors']:
+#        codegen_func.sensor_func_h_gen(sensor, template_dir, header_dir)
+#        codegen_func.sensor_func_cpp_gen(sensor, template_dir, src_dir)
+#        if sensor['power'] == True:
+#            codegen_func.sensor_power_h_gen(sensor, template_dir, header_dir)
+#            codegen_func.sensor_power_cpp_gen(sensor, template_dir, src_dir)
+#
+#    # Generate Load_converter files
+#    codegen_func.load_converter_h_gen(template_dir, header_dir)
+#    codegen_func.load_converter_cpp_gen(template_dir, src_dir)
 
     print ("OK!")
 
--- Simulation_v2/codegen/codegen_func.py
+++ Simulation_v2/codegen/codegen_func.py
@@ -1,104 +1,34 @@
 from jinja2 import Template
 
 __all__ = [
-    'bus_h_gen', 'bus_cpp_gen',
     'config_gen',
     'conv_sensor_h_gen', 'conv_sensor_cpp_gen',
     'main_cpp_gen',
-    'makefile_gen',
-    'mcu_cpp_gen',
-    'rf_cpp_gen',
     'sensor_h_gen', 'sensor_cpp_gen',
     'sensor_func_h'
 ]
 
-
-def bus_h_gen(settings, template_dir, output_dir):
-    with open(template_dir / 'h' / 'bus_h.txt') as temp:
-        template = Template(temp.read())
-        with open(output_dir / 'bus.h', 'w') as f:
-            active_sensors = settings['sensors']
-            sensor_names = []
-            for sensor in active_sensors:
-                sensor_names.append(sensor['name'])
-            f.write(template.render(active_sensors=sensor_names))
-
-
-def bus_cpp_gen(settings, template_dir, output_dir):
-    with open(template_dir / 'cpp' / 'bus_cpp.txt') as temp:
-        template = Template(temp.read())
-        with open(output_dir / 'bus.cpp', 'w') as f:
-            active_sensors = settings['sensors']
-            sensor_names = []
-            for sensor in active_sensors:
-                sensor_names.append(sensor['name'])
-            f.write(template.render(active_sensors=sensor_names))
-
-
 def config_gen(settings, template_dir, output_dir):
+    nsensor = 0
+    for sensor in settings["sensors"]:
+        nsensor += 1
+
     # define generic params
     with open(output_dir / 'config.h', 'w') as f:
-        f.write('// generic simulation settings\n')
+        f.write('//General Parameters\n')
         f.write(f'#define SIM_STEP {settings["sim_step"]}\n')
         f.write(f'#define SIM_LEN {settings["sim_len"]}\n')
-        f.write(f'#define PERIOD {settings["period"]}\n')
-        f.write(f'#define VREF_BUS {settings["vref_bus"]}\n')
         f.write(f'#define SOC_INIT {settings["soc_init"]}\n')
+        f.write(f'#define VREF_BUS {settings["vref_bus"]}\n')
         f.write(f'#define SELFDISCH_FACTOR {settings["selfdisch_factor"]}\n')
+        f.write(f'#define NS {nsensor + 1}\n')
+        f.write(f'#define NP {nsensor}\n')
 
     # define sensors' params
-    active_sensors = settings['sensors']
     with open(template_dir / 'h' / 'config_sensor.txt') as temp:
         template = Template(temp.read())
-        max_sensing_time = 0    # used to know when the MCU can start
-        for sensor in active_sensors:
-            sensor_name = sensor['name']
-            current_on = sensor['current_on']
-            current_idle = sensor['current_idle']
-            activation_time = sensor['activation_time']
-            time_on = sensor['time_on']
-            with open(output_dir / 'config.h', 'a') as f:
-                f.write(
-                    template.render(
-                        sensor_name=sensor_name,
-                        current_on=current_on,
-                        current_idle=current_idle,
-                        activation_time=activation_time,
-                        time_on=time_on))
-            sensing_time = int(activation_time) + int(time_on)
-            if sensing_time > max_sensing_time:
-                max_sensing_time = sensing_time
-
-    # define MCU's params
-    with open(template_dir / 'h' / 'config_MCU.txt') as temp:
-        template = Template(temp.read())
-        mcu = settings['mcu']
-        mcu_i_idle = mcu['current_idle']
-        states = mcu['states']
-        mcu_act_time = max_sensing_time + 1  # mcu activated when sensing ends
-        act_times = {}
-        for state in states:
-            val = int(mcu_act_time) + int(state['time_on']) + 1
-            act_times[state['name']] = val
         with open(output_dir / 'config.h', 'a') as f:
-            f.write(
-                template.render(
-                    mcu_i_idle=mcu_i_idle,
-                    mcu_act_time=mcu_act_time,
-                    states=states,
-                    act_times=act_times))
-
-    # define params for RF
-    with open(template_dir / 'h' / 'config_RF.txt') as temp:
-        template = Template(temp.read())
-        rf = settings['rf']
-        states = rf['states']
-        rf_i_idle = rf['current_idle']
-        with open(output_dir / 'config.h', 'a') as f:
-            f.write(
-                template.render(
-                    rf_i_idle=rf_i_idle,
-                    states=states))
+            f.write(template.render(sensors = settings["sensors"]))
 
 def main_cpp_gen(settings, template_dir, output_dir):
     with open(template_dir / 'cpp' / 'main_cpp.txt') as temp:
@@ -110,35 +40,6 @@ def main_cpp_gen(settings, template_dir, output_dir):
                 sensor_names.append(sensor['name'])
             f.write(template.render(active_sensors=sensor_names))
 
-
-def makefile_gen(settings, template_dir, output_dir):
-    with open(template_dir / 'Makefile' / 'Makefile.txt') as temp:
-        template = Template(temp.read())
-        with open(output_dir, 'w') as f:
-            active_sensors = settings['sensors']
-            sensor_names = []
-            sensor_id = 0
-            for sensor in active_sensors:
-                sensor_id += 1
-                sensor_names.append(sensor_id)
-            f.write(template.render(active_sensors=sensor_names))
-
-
-def mcu_cpp_gen(settings, template_dir, output_dir):
-    with open(template_dir / 'cpp' / 'mcu_cpp.txt') as temp:
-        template = Template(temp.read())
-        states = settings['mcu']['states']
-        with open(output_dir / 'mcu.cpp', 'w') as f:
-            f.write(template.render(states=states))
-
-
-def rf_cpp_gen(settings, template_dir, output_dir):
-    with open(template_dir / 'cpp' / 'rf_cpp.txt') as temp:
-        template = Template(temp.read())
-        states = settings['rf']['states']
-        with open(output_dir / 'rf.cpp', 'w') as f:
-            f.write(template.render(states=states))
-
 # Functional System Bus Methods
 
 def sys_functional_bus_h_gen(template_dir, output_dir):
@@ -147,12 +48,25 @@ def sys_functional_bus_h_gen(template_dir, output_dir):
         with open(output_dir / 'sys_functional_bus.h', 'w') as f:
             f.write(template.render())
 
+def sys_functional_bus_cpp_gen(settings, template_dir, output_dir):
+    with open(template_dir / 'cpp' / 'sys_functional_bus_cpp.txt') as temp:
+        template = Template(temp.read())
+        tmp_data = dict(settings)
+        with open(output_dir / 'sys_functional_bus.cpp', 'w') as f:
+            f.write(template.render(data=tmp_data))
+
 # Power System Bus Methods
 
 def sys_power_bus_h_gen(template_dir, output_dir):
     with open(template_dir / 'h' / 'sys_power_bus_h.txt') as temp:
         template = Template(temp.read())
         with open(output_dir / 'sys_power_bus.h', 'w') as f:
+            f.write(template.render())
+
+def sys_power_bus_cpp_gen(template_dir, output_dir):
+    with open(template_dir / 'cpp' / 'sys_power_bus_cpp.txt') as temp:
+        template = Template(temp.read())
+        with open(output_dir / 'sys_power_bus.cpp', 'w') as f:
             f.write(template.render())
 
 #Sensor Methods
