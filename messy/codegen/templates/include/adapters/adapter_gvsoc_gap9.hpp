dc2eba862d8de157938c9537603077f1727e3f52
Mohamed Amine Hamdi <mo.amine.hamdi@gmail.com>
2024-03-26 13:50:48 +0100

    fix: gap9 files

---
+++ messy/codegen/templates/include/adapters/adapter_gvsoc_gap9.hpp
@@ -0,0 +1,36 @@
+#ifndef __ADAPTER_GVSOC_H
+#define __ADAPTER_GVSOC_H
+
+#include <config.hpp>
+#include <gv/gvsoc.hpp>
+// vtable issues? how to build abstract class?
+//#include <adapters/iss_adapter.hpp>
+#include <cstring>
+#include <string.h>
+#include <vp/launcher.hpp>
+#include <vp/itf/io.hpp>
+#include <messy_request.hpp>
+
+class AdapterGvsocGap9 : public gv::Io_user, public vp::Notifier{
+    public:
+    int64_t exec_events_at(int64_t timestamp);
+    double get_power_at(int64_t timestamp);
+    void close();
+    void startup();
+    void custom_reply(MessyRequest* req);
+    void access(gv::Io_request* req);
+    void grant(gv::Io_request *req);
+    void reply(gv::Io_request *req);
+    void notify_stop(int64_t time);
+    void notify_run(int64_t time);
+    MessyRequest* get_messy_request_from_gvsoc(gv::Io_request* req);
+    AdapterGvsocGap9();
+    //~AdapterGvsocGap9() {};
+    bool finished=false;
+    private:
+    int closed=0;
+    gv::Io_binding *axi;
+    Gvsoc_launcher *gvsoc;
+};
+
+#endif
---
+++ messy/codegen/templates/src/adapters/adapter_gvsoc_gap9.cpp
@@ -0,0 +1,85 @@
+#include <adapters/adapter_gvsoc_gap9.hpp>
+
+AdapterGvsocGap9::AdapterGvsocGap9()
+{
+}
+void AdapterGvsocGap9::startup(){
+    string gvsoc_config_path="${config['path']}";
+    gv::GvsocConf conf;
+    conf.config_path=gvsoc_config_path;
+    conf.api_mode=gv::Api_mode::Api_mode_sync;
+    conf.proxy_socket=-1;
+    gvsoc = (Gvsoc_launcher*) gv::gvsoc_new(&conf);
+    gvsoc->open();
+    // register this core as an executor notifier
+    gvsoc->register_exec_notifier(this);
+
+    // Get a connection to the main soc AXI. This will allow us to inject accesses
+    // and could also be used to received accesses from simulated test
+    // to a certain mapping corresponding to the external devices.
+    this->axi = gvsoc->io_bind(this, ${config["axi_path"]}, "");
+    if (this->axi == NULL)
+    {
+        fprintf(stderr, "Couldn't find AXI proxy\n");
+        return;
+    }
+
+    gvsoc->start();
+}
+
+MessyRequest* AdapterGvsocGap9::get_messy_request_from_gvsoc(gv::Io_request* req){
+    return (MessyRequest*)new MessyRequest(0,0,true,0x0);
+}
+
+int64_t AdapterGvsocGap9::exec_events_at(int64_t timestamp){
+    int64_t next_event_time=gvsoc->step_until(timestamp);
+    finished=this->gvsoc->top_get()->time_engine->finished_get();
+    return next_event_time;
+}
+
+double AdapterGvsocGap9::get_power_at(int64_t timestamp){
+    double x=0,y=0;
+    return this->gvsoc->get_instant_power(x,y);
+}
+
+void AdapterGvsocGap9::close(){
+    if(!closed){
+        int retval = gvsoc->join();
+        //std::cout << gvsoc->stop() << std::endl;
+        gvsoc->stop();
+        gvsoc->close();
+        closed=1;
+    }
+}
+
+void AdapterGvsocGap9::access(gv::Io_request* req){
+    core_requests.emplace_back(this->get_messy_request_from_gvsoc(req));
+}
+
+void AdapterGvsocGap9::custom_reply(MessyRequest* req){
+    vp::io_req *vp_req = (vp::io_req *)req->handle_c;
+    vp_req->get_resp_port()->resp(vp_req);
+    delete req;
+}
+
+void AdapterGvsocGap9::grant(gv::Io_request *req)
+{
+
+}
+
+void AdapterGvsocGap9::reply(gv::Io_request *req)
+{
+
+}
+
+void AdapterGvsocGap9::notify_stop(int64_t time)
+{
+    if(!this->gvsoc->retain_count())
+        this->gvsoc->release();
+}
+
+void AdapterGvsocGap9::notify_run(int64_t time)
+{
+    if(!this->gvsoc->retain_count())
+        this->gvsoc->retain();
+}
