f505c168f974e2904e4e54bbc9782a51cb963d9d
Giovanni Pollo <giovannipollo98@gmail.com>
2024-09-11 15:42:46 +0200

    Commented and documented core_power, lut and power_bus

--- messy/codegen/templates/include/power_bus.hpp
+++ messy/codegen/templates/include/power_bus.hpp
@@ -1,32 +1,39 @@
-#include <systemc.h>
-#include <systemc-ams.h>
 #include <config.hpp>
+#include <systemc-ams.h>
+#include <systemc.h>
 
-SCA_TDF_MODULE(Power_bus){
-    //Rate 1ms
-    //Input Port
-    sca_tdf::sca_in <double>  core_current;
-    sca_tdf::sca_in <double>  core_voltage;
-    sca_tdf::sca_in <double>  voltage_sensors[NUM_SENSORS];
-    sca_tdf::sca_in <double>  current_sensors[NUM_SENSORS];
-    #if NUM_SOURCES>0
-    sca_tdf::sca_in <double>    current_sources[NUM_SOURCES];
-    #endif
-    #if NUM_BATTERIES>0
-    sca_tdf::sca_out <double>   current_batteries[NUM_BATTERIES];
-    #endif
+SCA_TDF_MODULE(Power_bus) {
+    // Time resolution is set to 1 ms, meaning the module is triggered every 1 ms
 
-    SCA_CTOR(Power_bus):
-    core_current("Current_of_CPU"),
-    core_voltage("Voltage_of_CPU")
-    {}
+    // Input Ports
+    sca_tdf::sca_in<double> core_current;
+    sca_tdf::sca_in<double> core_voltage;
+    sca_tdf::sca_in<double> voltage_sensors[NUM_SENSORS];
+    sca_tdf::sca_in<double> current_sensors[NUM_SENSORS];
+
+#if NUM_SOURCES > 0
+    sca_tdf::sca_in<double> current_sources[NUM_SOURCES];
+#endif
+#if NUM_BATTERIES > 0
+    sca_tdf::sca_out<double> current_batteries[NUM_BATTERIES];
+#endif
+
+    /**
+     * @brief Constructor for the Core_power module.
+     * 
+     * Initializes the input and output signal names for tracing and connecting to other modules.
+     */
+    SCA_CTOR(Power_bus) : core_current("Current_of_CPU"),
+                          core_voltage("Voltage_of_CPU") {}
 
     void set_attributes();
     void initialize();
     void processing();
 
-    private:
+  private:
     double total_current = 0;
 
-    Power_bus(){}
+    // Default constructor is private to prevent its use, ensuring that
+    // the module must be initialized using the public constructor above.
+    Power_bus() {}
 };
--- messy/codegen/templates/src/power_bus.cpp
+++ messy/codegen/templates/src/power_bus.cpp
@@ -1,14 +1,40 @@
 #include <power_bus.hpp>
 
-void Power_bus::set_attributes(){}
+void Power_bus::set_attributes() {}
 
-void Power_bus::initialize(){}
+void Power_bus::initialize() {}
 
-void Power_bus::processing(){
+/**
+ * @warning This function should not be called directly. It is called by the SystemC-AMS kernel.
+ *
+ * @brief This function calculates and updates the total current on the power bus.
+ *
+ * It reads the current values from the sensors, the core, and the sources.
+ * It sums the current from all sensors and core, and then subtracts the
+ * current from the power sources. If there are any batteries, the calculated
+ * total current is divided equally among the batteries and written to each
+ * battery.
+ *
+ * The function performs the following steps:
+ * - Initializes the `total_current` to 0.
+ * - Loops through the current sensors to read and accumulate their current values.
+ * - Reads the current from the core and adds it to `total_current`.
+ * - If there are any current sources, their values are read and subtracted from `total_current`.
+ * - If there are any batteries, the total current is divided by the number of batteries,
+ *   and the value is written to each battery.
+ *
+ * @note The number of sensors, sources, and batteries is determined by the constants
+ * `NUM_SENSORS`, `NUM_SOURCES`, and `NUM_BATTERIES` respectively.
+ *
+ * @warning If `NUM_SOURCES` or `NUM_BATTERIES` is 0, the respective section of code
+ * will not be executed.
+ *
+ */
+void Power_bus::processing() {
     double tmp_i;
     total_current = 0;
 
-    for (int i = 0; i < NUM_SENSORS; i++){
+    for (int i = 0; i < NUM_SENSORS; i++) {
         tmp_i = current_sensors[i].read();
         total_current += tmp_i;
     }
@@ -17,15 +43,15 @@ void Power_bus::processing(){
 
     total_current += tmp_i;
 
-    #if NUM_SOURCES>0
-    for (int i = 0; i < NUM_SOURCES; i++){
+#if NUM_SOURCES > 0
+    for (int i = 0; i < NUM_SOURCES; i++) {
         tmp_i = current_sources[i].read();
         total_current -= tmp_i;
     }
-    #endif
-    
-    #if NUM_BATTERIES>0
+#endif
+
+#if NUM_BATTERIES > 0
     for (int i = 0; i < NUM_BATTERIES; i++)
-        current_batteries[i].write(total_current/NUM_BATTERIES);
-    #endif
+        current_batteries[i].write(total_current / NUM_BATTERIES);
+#endif
 }
